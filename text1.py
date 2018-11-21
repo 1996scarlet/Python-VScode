4fc12ab2298029e8877c1f7c584cbf85979149d7
Scarlet <1996scarlet@gmail.com>
2018-11-21 14:18:05 +0800

    OJBK

--- text1.py
+++ text1.py
@@ -1,19 +1,32 @@
 #coding=utf-8
-import sys
-if __name__ == "__main__":
+# import sys
+# if __name__ == "__main__":
 
-    line = sys.stdin.readline().strip()
-    # 把每一行的数字分隔后转化成int列表
-    values = list(map(int, line.split()))
+#     line = sys.stdin.readline().strip()
+#     # 把每一行的数字分隔后转化成int列表
+#     values = list(map(int, line.split()))
 
-    n = values[0]
-    k = values[1]
+#     n = values[0]
+#     k = values[1]
 
-    line = sys.stdin.readline().strip()
-    # 把每一行的数字分隔后转化成int列表
-    values = list(map(int,line.split()))
-    # values = array(set(values))
+#     line = sys.stdin.readline().strip()
+#     # 把每一行的数字分隔后转化成int列表
+#     values = list(map(int,line.split()))
+#     # values = array(set(values))
 
-    newlit = [i + k for i in values]
+#     newlit = [i + k for i in values]
 
-    print(len(set(values) & set(newlit)))
+#     print(len(set(values) & set(newlit)))
+
+import numpy as np
+
+embeddings1 = [[1,2,3],[4,5,6],[7,8,9]]
+embeddings2 = [[2,2,2],[2,2,2],[2,2,2]]        
+diff = np.subtract(embeddings1, embeddings2)
+
+print (diff)
+dist = np.sum(np.square(diff), 1)
+print (np.square(diff))
+print (np.sum(np.square(diff), 0))
+
+print (dist)
