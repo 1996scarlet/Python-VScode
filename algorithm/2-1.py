3c96d6cd45af902b961e5b2a53e0ebf258ad55e6
1996scarlet <1996scarlet@gmail.com>
2018-11-28 21:17:42 +0800

    sss

--- algorithm/2-1.py
+++ algorithm/2-1.py
@@ -1,21 +1,34 @@
 import os, tarfile
 
 
-def unpack_path_file(pathname):
+def unpack_path_file(pathname,mdir):
     archive = tarfile.open(pathname, 'r:gz')
+    adir = ''
     for tarinfo in archive:
         if tarinfo.isfile() and tarinfo.name.endswith('gz'):
-            archive.extract(tarinfo, os.getcwd())
-            print('tarinfo', tarinfo.name)
-            unpack_path_file(tarinfo.name)
+            print('tarinfo', mdir + '/' + tarinfo.name, adir)
+            archive.extract(tarinfo, mdir)
+            unpack_path_file(mdir + '/' + tarinfo.name, mdir + '/' + adir)
+        elif tarinfo.isdir():
+            adir = tarinfo.name
+        else:
+            archive.extract(tarinfo, './ojbk/' + mdir)
+            # archive.extract(tarinfo, './ojbk')
     archive.close()
 
+# unpack_path_file('00.tar.gz', '.')
+counter = 0
+summer = 0
+for root, dirs, files in os.walk("./ojbk", topdown=False):
+    for name in files:
+        with open(os.path.join(root, name)) as f:
+            print(os.path.join(root, name))
+            ss = f.readline()
+            if(ss != ''):
+                print(ss)
+                summer += int(ss)
+            counter += 1
 
-# def tarker(pathname):
-
-# archive = tarfile.open('./00.tar.gz', 'r:gz')
-# archive.extractall()
-# archive.close()
-
-# unpack_path_file('00.tar.gz')
-unpack_path_file('00.tar.gz')
+print (counter, summer)
+    # for name in dirs:
+        # print(os.path.join(root, name))
